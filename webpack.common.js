657db5d1e51e890d2dd919fffa2e95163d44bc49
yoonjoohye <67yjh00@gmail.com>
2020-07-20 15:37:54 +0900

    [fix] emotion css props 에러

--- webpack.common.js
+++ webpack.common.js
@@ -23,13 +23,9 @@ module.exports = {
                         presets: [
                             '@babel/preset-env',
                             '@babel/preset-react',
-                            '@babel/preset-typescript'
-                        ],
-                        plugins: [
-                            '@babel/proposal-class-properties',
-                            '@babel/proposal-object-rest-spread',
+                            '@babel/preset-typescript',
                             [
-                                'emotion',
+                                '@emotion/babel-preset-css-prop',
                                 {
                                     sourceMap: true,
                                     autoLabel: true,
@@ -37,6 +33,11 @@ module.exports = {
                                     cssPropOptimization: true
                                 }
                             ]
+                        ],
+                        plugins: [
+                            '@babel/proposal-class-properties',
+                            '@babel/proposal-object-rest-spread',
+
                         ]
                     }
                 },
